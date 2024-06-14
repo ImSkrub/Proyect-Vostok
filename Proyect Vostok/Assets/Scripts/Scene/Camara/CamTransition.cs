46b3854547bc0a5f7b0b78a2311f0fd758e859f6
ImSkrub <estebanaberastury@gmail.com>
2024-06-14 17:44:44 -0300

    Fixes tamaño y agregado camara fija

--- Proyect Vostok/Assets/Scripts/Scene/Camara/CamTransition.cs
+++ Proyect Vostok/Assets/Scripts/Scene/Camara/CamTransition.cs
@@ -6,13 +6,13 @@ public class CamTransition : MonoBehaviour
 {
     public Camera cam;
     public GameObject positions;
- 
+  
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-          cam.transform.Translate(positions.transform.position);
+          cam.transform.position=positions.transform.position;
         }
     }
 
