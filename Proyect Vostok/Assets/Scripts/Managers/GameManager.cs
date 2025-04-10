eb9f2844f170fc6f92997e229a79221f6a2e5287
ImSkrub <estebanaberastury@gmail.com>
2025-04-09 23:18:43 -0300

    Level manager fix
    Todavia no funciona el level select

--- Proyect Vostok/Assets/Scripts/Managers/GameManager.cs
+++ Proyect Vostok/Assets/Scripts/Managers/GameManager.cs
@@ -4,25 +4,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
-    [Header("Copia")]
+    [Header("Jugador y Copia")]
     [SerializeField] GameObject player;
     [SerializeField] GameObject copiaPlayerPrefab;
     [SerializeField] int copyLimit = 10;
    
-    //Beta cambiar a queue
-    public static List<Vector3> playerPositions = new List<Vector3>();
-  
     public static Queue<List<Vector3>> _listPlayerPositions = new Queue<List<Vector3>>();
     public static Queue<GameObject> _copiaPlayers = new Queue<GameObject>();
 
- 
-    
-    private void Awake()
+     private void Awake()
     {
         if (Instance != null)
         {
@@ -33,15 +29,18 @@ public class GameManager : MonoBehaviour
             Instance = this;
             DontDestroyOnLoad(gameObject);
         }
-        player.GetComponent<PlayerLife>().OnDeath += FinishGame;
-        
+        if (player != null && player.TryGetComponent(out PlayerLife playerLife))
+        {
+            playerLife.OnDeath += FinishGame;
+        }
+
     }
     // Update is called once per frame
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            SceneManager.LoadScene(0);
+           LevelManager.instance.LoadMainMenu();
             ResetList();
             
         }
@@ -68,23 +67,6 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    //public void UpdateListOfPositions(List<CopyDataModel> listPos)
-    //{
-    //    var listPositions = new List<Vector3>();
-    //    if (listPos != null)
-    //    {
-    //        foreach (var item in listPos)
-    //        {
-    //            listPositions.Add(item.Pos);
-    //        }
-    //    }
-    //    listPlayerPositions.Add(listPositions);
-    //    print(listPlayerPositions.Count);
-    //    copiaPlayers.Add(new GameObject("copia" + counter.ToString()));
-    //    print(copiaPlayers.Count);
-    //    counter++;
-    //}
-
     public void _Reset()
     {
         foreach (var copia in _copiaPlayers)
@@ -96,42 +78,46 @@ public class GameManager : MonoBehaviour
 
     private void instantiateCopiaPlayer(GameObject player, List<Vector3> listOfPositions)
     {
-        print(listOfPositions.Count);
-        print(_copiaPlayers.Count);
-        player = Instantiate(copiaPlayerPrefab);
-        player.TryGetComponent<PlayerCopia>(out PlayerCopia copiaPlayer);
-        copiaPlayer.setListOfPositions(listOfPositions);
+        //player = Instantiate(copiaPlayerPrefab);
+        //player.TryGetComponent<PlayerCopia>(out PlayerCopia copiaPlayer);
+        //copiaPlayer.setListOfPositions(listOfPositions);
+        var copia = Instantiate(copiaPlayerPrefab);
+        if (copia.TryGetComponent(out PlayerCopia copiaPlayer))
+        {
+            copiaPlayer.setListOfPositions(listOfPositions);
+        }
     }
 
     public void instantiateListOfObjects()
     {
-        print(_copiaPlayers.Count);
-        print(_listPlayerPositions.Count);
-        for (int i = 0; i < _copiaPlayers.Count; i++)
+        int count = _copiaPlayers.Count;
+        for (int i = 0; i < count; i++)
         {
             GameObject copia = _copiaPlayers.Dequeue();
             List<Vector3> listPos = _listPlayerPositions.Dequeue();
+
             instantiateCopiaPlayer(copia, listPos);
+            
             _copiaPlayers.Enqueue(copia);
             _listPlayerPositions.Enqueue(listPos);
-
         }
     }
 
     public void ResetList()
     {
         _listPlayerPositions.Clear();
+        
         foreach (var copia in _copiaPlayers)
         {
             Destroy(copia);
         }
         _copiaPlayers.Clear();
-        playerPositions.Clear();
+
         PowerUpManager.Instance.powerUpDisabled.Clear();
         PowerUpManager.Instance.activePowerUps.Clear();
     }
 
-    public Queue<GameObject> getCopiaPlayers() { return _copiaPlayers; }
+    public Queue<GameObject> getCopiaPlayers() => _copiaPlayers;
 
     void FinishGame()
     {
@@ -149,7 +135,6 @@ public class GameManager : MonoBehaviour
         if (LevelManager.instance.currentLevel >= 1)
         {
             copyLimit = newCopyLimit;
-        }
-              
+        }         
     }
 }
